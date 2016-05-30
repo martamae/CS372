91d74c727ba78b1402e3dbb2197f6390dca79315
martamae <wegnerma@onid.oregonstate.edu>
2016-05-29 22:39:42 -0500

    Project 2 - list function works fully, file function works up to transfer
    File function: fully transfer file, port info
    Unable to send length of file from server to client

---
+++ Projects/Project2/ftclient.py
@@ -0,0 +1,336 @@
+# Name: Marta Wegner
+# CS 372
+# Program #2
+#
+# Description:
+# 
+# References:
+# http://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+# http://www.tutorialspoint.com/python/python_networking.htm
+# http://www.tutorialspoint.com/python/os_listdir.htm
+# http://www.tutorialspoint.com/python/python_files_io.htm
+# http://www.tutorialspoint.com/python/python_command_line_arguments.htm
+
+#!/bin/python
+
+import socket
+import sys
+import os
+
+
+# Function: initiateContact()
+# Initiates contact with the server (sets up control connection)
+#
+# Parameters:
+#	serverHost: host that the server is on
+#	serverPort: port number to connect to the server on
+#
+# Returns: 
+#	controlSocket on successful connection
+#	-1 on failed connection
+def initiateContact(serverHost, serverPort):
+	#create socket
+	controlSocket = socket.socket()
+	if (controlSocket == -1):
+		return -1
+
+	#connect
+	if ((controlSocket.connect((serverHost, serverPort))) == -1):
+		return -1
+
+	return controlSocket
+
+
+# Function: makeRequest()
+# Makes request to server for list of directories or file
+# over the control connection
+# -l command makes request for list
+# -g command makes request for file
+# 
+# Parameters:
+#	request type
+#		0 for -l command
+#		1 for -g command
+#	filename
+#		filename of file to request from server
+#	dataPort
+#		port number that server should establish the data
+#		connection on
+#
+# Returns:
+#	0 on succesful request
+#	-1 on failed request
+def makeRequest(controlSocket, requestType, dataPort, filename):
+	ACK = 0
+	if (requestType == 0): #request directory list
+		#send request type
+		if (controlSocket.send(str(requestType)) == -1):
+			#error sending requestType
+			print >> sys.stderr, 'ERROR: error sending requestType\n\n'
+			return -1
+
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+
+		#send dataPort
+		dataPort = dataPort + '\0' #null terminate
+		if (controlSocket.send(dataPort) == -1):
+			#error sending dataPort
+			print >> sys.stderr, 'ERROR: error sending dataPort\n\n'
+			return -1
+
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+
+		#send client host
+		#get hostname
+		clientHost = socket.gethostname()
+
+		if(controlSocket.send(clientHost) == -1):
+			#error sending hostname
+			print >> sys.stderr, 'ERROR: error sending hostname\n\n'
+			return -1
+
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+	elif (requestType == 1): #request file
+		#send request type
+		if (controlSocket.send(str(requestType)) == -1):
+			#error sending request type
+			print >> sys.stderr, 'ERROR: error sending requestType\n\n'
+			return -1
+
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+
+		#send dataPort
+		dataPort = dataPort + '\0' #null terminate
+		if (controlSocket.send(dataPort) == -1):
+			#error sending dataPort
+			print >> sys.stderr, 'ERROR: error sending dataPort\n\n'
+			return -1
+
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+
+		#send client host
+		#get hostname
+		clientHost = socket.gethostname()
+
+		if(controlSocket.send(clientHost) == -1):
+			#error sending hostname
+			print >> sys.stderr, 'ERROR: error sending hostname\n\n'
+			return -1
+
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+
+		#null terminate filename to send
+		filename = filename + '\0'
+		
+		#send filename
+		if (controlSocket.send(filename) == -1):
+			#error sending filename
+			print >> sys.stderr, 'ERROR: error sending filename\n\n'
+			return -1
+		
+		#receive acknowledgement
+		ACK = controlSocket.recv(4)
+	else:
+		#invalid request type
+		print >> sys.stderr, 'ERROR: error making request to server'
+		return -1
+
+	return 0
+
+# receiveFile()
+# Receives file from the server on the dataPort
+#
+# Parameter:
+#		serverDataSocket: socket to receive file over
+#		requestType: type of request
+#			0 for directory request
+#			1 for file request
+#		filename: 
+#			name of file to receive or -1 when 
+#			directory is being received
+#
+# Returns:
+#	-1 for failure
+#	0 for success
+def receiveFile(serverDataSocket, requestType, filename):
+	if (requestType == 0): #directory
+		#while there are still file names from the directory to receive
+		
+		r = 1
+		print '\nDirectory Listing:'
+		while(r): #while there is more info to receive
+			#receive
+			r = serverDataSocket.recv(40)
+
+			if (r == '-1'):
+				#error receiving directory
+				print >> sys.stderr, 'ERROR: unable to receive directory\n\n'
+				return -1
+			elif (r == 0): 
+				#if end of directory
+				return 0
+			else:
+				#if directory file received received, print
+				print '  ' + r
+
+				#send ACK
+				ACK = 'ACK'
+				serverDataSocket.send(ACK)
+	elif (requestType == 1): #file
+		#receive length of file
+		len = serverDataSocket.recv(15)
+
+		#send ACK
+		ACK = 'ACK'
+		serverDataSocket.send(ACK)
+
+		#if error receiving file
+		fileLength = int(len)
+		
+		#Error opening/sending file
+		if (fileLength == "-1"):
+			print >> sys.stderr, 'ERROR: file unable to be sent\n\n'
+			return -1
+
+		#compare name of file to files in directory
+		dir = os.listdir('.')
+
+		for file in dir:
+			if (file == filename): 
+				#if filename is already in directory rename
+				pid = os.getpid()
+				filename = filename + pid
+
+		# open file for appending
+		file = open(filename, "a")
+
+		#receive file
+		len = 0 #length received
+		while (len < fileLength): #while len received < total len expected
+			#receive
+			r = serverDataSocket.recv(1024)
+
+			if (r == -1):
+				#if error receiving file
+				print >> sys.stderr, 'ERROR: error receiving file\n\n'
+				return -1
+			elif (r == 0):
+				#if end of file
+				return 0
+			else:
+				#add received portion to file
+				file.write(r)
+
+		#send ACK
+		ACK = 'ACK'
+		serverDataSocket.send(ACK)
+	
+	return 0
+
+
+# Function: main
+def main(argv):
+	#Validates command line parameters
+	if (len(sys.argv) < 5):
+		print >> sys.stderr, 'ERROR: Too few arguments included\n\n'
+		sys.exit(1)
+	
+	#set up connection
+	serverHost = argv[0]
+	serverPort = int(argv[1])
+
+	#initiate contact
+	controlSocket = initiateContact(serverHost, serverPort)
+
+	if(controlSocket == -1):
+		#if error connecting to server
+		print >> sys.stderr, 'ERROR: error connection to server\n\n'
+		sys.exit(1)
+	
+	dataPort = 0
+	requestType = -1
+	filename = -1
+	if (argv[2] == '-l'): #If list command
+		if (len(sys.argv) != 5):
+			#If the wrong number of arguments are included
+			print >> sys.stderr, 'ERROR: ftclient.py <SERVER_HOST><SERVER_PORT> -l <DATA_PORT>\n\n'
+			sys.exit(1)
+	
+		#requestType
+		requestType = 0;
+
+		#get dataPort
+		dataPort = argv[3]
+
+		#Ask for directory list
+		makeRequest(controlSocket, requestType, dataPort, -1)
+	elif (argv[2] == '-g'): #If get command
+		if (len(sys.argv) != 6):
+			#If the wrong number of args included
+			print >> sys.stderr, 'ERROR: ftclient.py<SERVER_HOST> <SERVER _PORT> -g <FILENAME> <DATA_PORT>\n\n'
+		
+		#requestType
+		requestType = 1
+
+		#get filename
+		filename = argv[3]
+
+		#get dataPort
+		dataPort = argv[4]
+
+		#Ask for file
+		makeRequest(controlSocket, requestType, dataPort, filename)
+	else: #else invalid command
+		#print error msg
+		print >> sys.stderr, 'ERROR: invalid command\n\n'
+		sys.exit(1)
+
+	#set up data connection
+	#create
+	dataSocket = socket.socket()
+	#error check
+	if (dataSocket == -1):
+		print >> sys.stderr, 'ERROR: error creating data socket\n\n'
+		exit(1)
+
+	#convert data port to int
+	dataPort = int(dataPort)
+
+	#bind
+	x = dataSocket.bind(('0.0.0.0', dataPort))
+	#error check
+	if(x == -1):
+		print >> sys.stderr, 'ERROR: error binding data socket\n\n'
+		exit(1)
+
+	#listen
+	x = dataSocket.listen(5)
+	#error check
+	if (x == -1):
+		print >> sys.stderr, 'ERROR: listening failed for data socket\n\n'
+		exit(1)
+
+	#accept
+	serverDataSocket, addr = dataSocket.accept()
+	#error check
+	if(serverDataSocket == -1):
+		print >> sys.stderr, 'ERROR: error accepteding data socket\n\n'
+
+	receiveFile(serverDataSocket, requestType, filename)
+
+	serverDataSocket.close
+
+	dataSocket.close
+
+	controlSocket.close
+
+	return 0
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
---
+++ Projects/Project2/ftserver.c
@@ -0,0 +1,350 @@
+/**********************************************************
+* Name: Marta Wegner
+* CS 372
+* Program #2
+*
+* Description:
+*
+* References:
+*	http://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
+**********************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/wait.h>
+#include<fcntl.h>
+#include<netinet/in.h>
+#include<dirent.h>
+#include<netdb.h>
+
+/**********************************************************
+*					startup()
+*	Starts up the socket on serverPort - control connection
+*	creates,bind, and listens for connections
+*	
+*	Parameters:
+*		serverPort: port to start control connection on
+*
+*	Returns:
+*		returns -1 on failed connection
+*		returns controlSocket on successful connection
+**********************************************************/
+int startup (int serverPort) {
+	int controlSocket;
+
+	//Control connection
+	//Create socket for control connection
+	if((controlSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {//Create
+		//If error creating print msg
+		fprintf(stderr, "ERROR: server socket creation\n");
+		return -1;
+	}
+
+	//Fill addr struct
+	struct sockaddr_in server;
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(serverPort);
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	//Bind socket to port
+	if(bind(controlSocket, (struct sockaddr *) &server, sizeof(server)) == -1) {//bind
+		//If bind error print msg
+		fprintf(stderr, "ERROR: server bind call failed\n");
+		return -1;
+	}
+
+	//Listen for connections
+	if (listen(controlSocket, 5) == -1) { //listen
+		//If listen call error print msg
+		fprintf(stderr, "ERROR: server listen call failed\n");
+		return -1;
+	}
+
+	return controlSocket;
+}
+
+int handleRequest(int clientControlSocket) {
+	char reqType[2];
+	int requestType;
+	char data[10];
+	int dataPort;
+	char clientHostName[255];
+	struct hostent * clientHost;
+	char filename[255];
+	char *ACK = "ACK";
+	char rcvACK[4];
+
+	//Receive request type from client
+	if((recv(clientControlSocket, &reqType, sizeof(reqType), 0)) == -1) {
+		//Error receiving requestType
+		fprintf(stderr, "ERROR: error receiving request type from client\n\n", reqType);
+		return -1;
+	}
+	
+	//acknowledge
+	if ((send(clientControlSocket, ACK, sizeof(ACK), 0)) == -1){
+		//error Acknowledging
+		fprintf(stderr, "ERROR: error acking request type");
+	}
+
+	//convert request type to int
+	requestType = atoi(reqType);
+
+	//Receive data port from client
+	if((recv(clientControlSocket, &data, sizeof(data), 0)) == -1) {
+		//Error receiving dataPort
+		fprintf(stderr, "ERROR: error receiving data port from client\n\n");
+		return -1;
+	}
+	
+	//acknowledge
+	if ((send(clientControlSocket, ACK, sizeof(ACK), 0)) == -1){
+		//error Acknowledging
+		fprintf(stderr, "ERROR: error acking data port");
+	}
+	
+	//convert data port
+	dataPort = atoi(data);
+
+	//Receive hostname from client
+	if((recv(clientControlSocket, &clientHostName, sizeof(clientHostName), 0)) == -1) {
+		//Error receiving client host
+		fprintf(stderr, "ERROR: error receiving client host\n\n");
+		return -1;
+	}
+	
+	//acknowledge
+	if ((send(clientControlSocket, ACK, sizeof(ACK), 0)) == -1){
+		//error Acknowledging
+		fprintf(stderr, "ERROR: error acking data port");
+	}
+
+	//get host from hostname
+	clientHost = gethostbyname(clientHostName);
+	
+	if (requestType == 1) { //if file request
+		//Receiving the file name
+		if((recv(clientControlSocket, &filename, sizeof(filename), 0)) == -1) {
+			//Error receiving filename
+			fprintf(stderr, "ERROR: error receiving filename from client\n\n");
+			return -1;
+		}
+		printf ("%s\n", filename);
+		//acknowledge
+		if ((send(clientControlSocket, ACK, sizeof(ACK), 0)) == -1){
+			//error Acknowledging
+			fprintf(stderr, "ERROR: error acking data port");
+		}
+	}
+	
+	//set up data socket
+	//create socket
+	int dataSocket;
+
+	if((dataSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) { //create
+		//If error creating
+		fprintf(stderr, "ERROR: error creating data socket\n\n");
+		return -1;
+	}
+
+	//set up address
+	struct sockaddr_in client;
+
+	//clear socket structure
+	memset((char *)&client, 0, sizeof(client));
+
+	client.sin_family = AF_INET;
+	client.sin_port = htons(dataPort);
+	memcpy(&client.sin_addr, clientHost->h_addr, clientHost->h_length);
+
+	//Connect socket
+	if(connect(dataSocket, (struct sockaddr*) &client, sizeof(client)) == -1) {
+		fprintf(stderr, "ERROR: error connecting data port\n\n");
+		return -1;
+	}
+
+	if(requestType == 1) {//if file request
+		//send file
+		//open file for reading
+		int file;
+		if((file = open(filename, O_RDONLY)) == -1) {
+			//error opening file
+			fprintf(stderr, "ERROR: error opening file\n\n");
+			
+			char * error = "-1";
+			//send error signal to client
+			send(dataSocket, &error, 2, 0);
+
+			return -1;
+		}
+
+		//Get size of file
+		int fileLength = lseek(file, 0, SEEK_END);
+
+		//Convert size to strinf
+		char lenStr[15];
+
+		//fill lenStr w/ null chars
+		memset((char *)&lenStr, '\0', sizeof(lenStr));
+
+		//convert length to string
+		snprintf(lenStr, fileLength, "%d", 15);
+
+		printf("%s\n", lenStr);
+		//send file text size
+		if (send(dataSocket, &lenStr, sizeof(lenStr), 0) == -1) { //send
+			//If error sending length
+			fprintf(stderr, "ERROR: error sending file length\n\n");
+			return -1;
+		}
+
+		//receive ACK
+		recv(dataSocket, &rcvACK, sizeof(rcvACK), 0);
+
+		//Create string to hold file
+		char *fileText = malloc(sizeof(char) * fileLength);
+
+		//Set file pointer to beginning of file
+		lseek(file, 0, SEEK_SET);
+
+		//Read file text into string
+		if(read(file, fileText, fileLength) == -1) { //read
+			//If error reading
+			fprintf(stderr, "ERROR: error reading cipher text\n\n");
+			return -1;
+		}
+
+		//send file
+		int len = 0; //length sent
+		while (len < fileLength) { //while whole file hasn't
+								   //been sent
+			char fileSend[1024]; //hold subset
+
+			//copy subset of string to send
+			strncpy(fileSend, &fileText[len], 1024);
+
+			//send
+			if (send(dataSocket, fileSend, 1024, 0) == -1) {
+				//If error sending
+				fprintf(stderr, "ERROR: error sending file\n\n");
+				return -1;
+			}
+
+			len += 1024; //add length sent to len
+		}
+
+		//receive ACK
+		recv(dataSocket, &rcvACK, sizeof(rcvACK), 0);
+
+		free(fileText);
+	}
+	else if(requestType == 0) {//if directory request
+		//send directory
+		//open directory for reading
+		DIR *dir;
+		if((dir = opendir(".")) == NULL) {
+			//error opening directory
+			fprintf(stderr, "ERROR: error opening directory\n\n");
+			
+			//send error signal to client
+			send(dataSocket, "-1", 2, 0);
+
+			return -1;
+		}
+
+		//read from directory and send
+		//send directory
+		struct dirent *d;
+		while ((d = readdir(dir)) != NULL) { //while whole file hasn't
+											//been sent
+			char name[40];
+
+			//fill name w/ null chars
+			memset((char *)&name, '\0', sizeof(name));
+
+			//copy name to name variable
+			strncpy(name, d->d_name, strlen(d->d_name));
+
+			printf("%s\n", name);
+
+			//if name == . or name == .. ignore
+			if (strncmp(name, ".", sizeof(name)) == 0 || strncmp(name, "..", sizeof(name)) == 0){
+				//do nothing
+			}
+			else { //else send
+				//send
+				if (send(dataSocket, name, sizeof(name), 0) == -1) {
+					//If error sending
+					fprintf(stderr, "ERROR: error sending directory name\n\n");
+					return -1;
+				}
+
+				//receive ACK
+				recv(dataSocket, &rcvACK, sizeof(rcvACK), 0);
+			}
+		}
+
+		//close directory
+		closedir(dir);
+	}
+
+	close(dataSocket);
+	return 0;
+}
+
+/**********************************************************
+*						main()
+**********************************************************/
+int main(int argc, char ** argv) {
+	int serverPort;
+	int controlSocket;
+	int clientControlSocket;
+
+	//if port number not specified
+	if (argc != 2) {
+		//Print error
+		fprintf(stderr, "ERROR: You must include a port number\n\n");
+		exit(1);
+	}
+	else {
+		//get SERVER_PORT from args
+		serverPort = atoi(argv[1]);
+	}
+
+	//start up
+	if ((controlSocket = startup(serverPort)) == -1) {
+		//Error in start up
+		exit(1);
+	}
+
+	//loop and accept
+	while (1) { //loop
+		//accept
+		clientControlSocket = accept(controlSocket, NULL, NULL);
+		if (clientControlSocket == -1) {
+			//If accept fails print msg
+			fprintf(stderr, "ERROR: Accept call failed\n");
+			exit(1);
+		}
+
+		int request;
+		request = handleRequest(clientControlSocket);
+
+		if (request == -1) {
+			//Error receiving or connecting	
+		}
+		else if (request == -2) { 
+			//Error opening or sending file
+		}
+
+		close(clientControlSocket);
+	}
+
+	close(controlSocket);
+
+	return 0;
+}
